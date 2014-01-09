bf35d32a14549d509b023b9a5f46d7fc3e5f2262
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-09 14:56:45 +0700

    add project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Photo::Application.config.secret_key_base = '68e950c6b0d0106c69e6e8fbfc5fd095f76c404255c1d0a1b8e3cb23e5b712c97e2f81c72aadcb7c4253ee96376884423ef6ee9801f376765dade10abc90fc4d'
