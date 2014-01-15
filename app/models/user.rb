7f99dd04f4b5cadafd2614ffd7507522ee3fb458
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-15 16:18:41 +0700

    inspect nil

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,15 +8,9 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :remember_me
   attr_accessible :nickname, :provider, :url, :username
 
-  def self.find_for_facebook_oauth access_token
-    if user = User.where(:url => access_token.info.urls.Facebook).first
-      user
-    else 
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Facebook, :username => access_token.extra.raw_info.name, :nickname => access_token.extra.raw_info.username, :email => access_token.extra.raw_info.email, :password => Devise.friendly_token[0,20]) 
-    end
-  end
 
   def self.find_for_vkontakte_oauth access_token
+    puts access_token.extra.raw_info.domain.inspect
     if user = User.where(:url => access_token.info.urls.Vkontakte).first
       user
     else 
