b8d56eb5a348adeb18ce2385db54b13e47fecd12
Feanaur <feanaur2193@gmail.com>
2014-01-15 17:05:51 +0700

    User.inspect debug

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ActiveRecord::Base
   def self.find_for_vkontakte_oauth access_token
     puts access_token.extra.raw_info.domain.inspect
     if user = User.where(:url => access_token.info.urls.Vkontakte).first
+      puts user.inspect
       user
     else 
       User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :password => Devise.friendly_token[0,20]) 
