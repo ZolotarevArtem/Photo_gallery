fa59c2ec07bd76358b658a52d6e07efeab30be6f
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-23 12:38:45 +0700

    fix email

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,8 +1,6 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def vkontakte
-    puts request.env['omniauth.auth'].inspect
     @user = User.find_for_vkontakte_oauth request.env["omniauth.auth"]
-
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Vkontakte"
       sign_in_and_redirect @user, :event => :authentication
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,15 @@ class User < ActiveRecord::Base
 
 
   def self.find_for_vkontakte_oauth access_token
-    puts access_token.info.name
-    if user = User.where(:url => access_token.info.urls.Vkontakte).first
+    if user = User.where(url:access_token.info.urls.Vkontakte).first
       user
     else 
-      User.create!(:provider => access_token.provider, :url => access_token.info.urls.Vkontakte, :username => access_token.info.name, :nickname => access_token.extra.raw_info.domain, :email => access_token.extra.raw_info.domain+'@vk.com', :password => Devise.friendly_token[0,20])
+      user = User.create( provider:access_token.provider, 
+                          url:access_token.info.urls.Vkontakte,
+                          username:access_token.info.name,
+                          nickname:access_token.extra.raw_info.domain,
+                          email:access_token.uid+'@vk.com',
+                          password: => Devise.friendly_token[0,20] )
     end
   end
   
