0ec6235be4debf276525908fcefffc4954008e61
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-28 12:19:41 +0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+user = User.create( provider:"Vkontakte"
+                    url:"http://vk.com/id226743771"
+                    uid:"226743771",
+                    username:"Александр Пистолетов",
+                    nickname:"nil",
+                    email:"226743771@vk.com",
+                    image: "http://cs412918.vk.me/v412918771/5259/kQiZrXkGd_U.jpg",
+                    roles:[:editor] )
