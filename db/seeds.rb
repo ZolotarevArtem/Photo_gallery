64370b627ce963a1dbbc438c0ad5d4a573ca2116
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-28 12:31:20 +0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
-user = User.create( provider:"Vkontakte"
-                    url:"http://vk.com/id226743771"
-                    uid:"226743771",
-                    username:"Александр Пистолетов",
-                    nickname:"nil",
-                    email:"226743771@vk.com",
+user = User.create( provider: "Vkontakte",
+                    url: "http://vk.com/id226743771",
+                    uid: "226743771",
+                    username: "Александр Пистолетов",
+                    nickname: "nil",
+                    email: "226743771@vk.com",
                     image: "http://cs412918.vk.me/v412918771/5259/kQiZrXkGd_U.jpg",
                     roles:[:editor] )
