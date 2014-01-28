0f95d51dfec2e57a7280eafa6132f9b07296f3a4
Rebelition <rebelition@rebelition-MS-16G1.(none)>
2014-01-28 12:41:46 +0700

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,5 @@ user = User.create( provider: "Vkontakte",
                     nickname: "nil",
                     email: "226743771@vk.com",
                     image: "http://cs412918.vk.me/v412918771/5259/kQiZrXkGd_U.jpg",
+                    password:Devise.friendly_token[0,20]
                     roles:[:editor] )
